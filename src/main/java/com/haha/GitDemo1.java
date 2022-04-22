ee7d7037096d22c0d50423ec5088bf9e2edfd1c5
Viceyo1 <90039714+Viceyo1@users.noreply.github.com>
2022-04-22 14:21:36 +0800

    pull commit

--- src/main/java/com/haha/GitDemo1.java
+++ src/main/java/com/haha/GitDemo1.java
@@ -8,5 +8,7 @@ public class GitDemo1 {
         System.out.println("hello git 111");
         System.out.println("冲突2");
         System.out.println("push test");
+        System.out.println("pull test");
+        
     }
 }
